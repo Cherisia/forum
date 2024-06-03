bf0ed888c11c7281deeca7a0efbb8298151f27b1
Cherisia <yhcyoon@kakao.com>
2024-06-03 16:48:59 +0900

    다이나믹 렌더링 설정, 캐싱 설정 예약변수 (dev는 적용안됨)

--- app/list/page.js
+++ app/list/page.js
@@ -1,6 +1,12 @@
 import {connectDB} from "@/util/database";
 import ListItem from "@/app/list/ListItem";
 
+/* 캐싱 설정 예약변수 (초단위) */
+export const revalidate = 60
+
+/* 강제로 다이나믹 랜더링 설정 예약변수 */
+export const dynamic = 'force-dynamic'
+
 export default async function List() {
     const db = (await connectDB).db('forum');
     let result = await db.collection('post').find().toArray();
